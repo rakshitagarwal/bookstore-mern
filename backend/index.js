48e7c36ac6da48943d96d8c95398c7d19d029fa5
rakshit agarwal <rakshitag13@gmail.com>
2024-02-12 22:05:20 +0530

    mern CRUD of books

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import bookRoutes from "./routes/booksRoute.js";
+import cors from "cors";
+const app = express();
+app.use(express.json());
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("welcome to MERN project");
+});
+
+app.use("/books", bookRoutes);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`Server is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
